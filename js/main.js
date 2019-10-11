d02cb4e506c12a4d1d24b10414c64872d1811087
SirRicc <patrick-pesch@hotmail.de>
2019-10-11 09:39:44 +0200

    Update main.js
    better structure - no code repetition

--- js/main.js
+++ js/main.js
@@ -1,49 +1,42 @@
 $(document).ready(function() {
 
+  const title1 = "Was ich gelernt habe:";
+  const description1 = "Ich habe gelernt unter welchen Vorraussetzungen eine kommerzielle Website höhere Chanchen hat in diversen Suchmaschinen ein höheres Rating zu bekommen um mit mehr Klicks mehr Einkommen zu generieren sowie Grundlegende Kentnisse die benötig werden um einfache aber wirkende Seiten zu gestalten.";
+
+  const title2 = "Arbeiten in der Web-Entwicklung:";
+  const description2 = "In unserem Ausflug in ein Unternehmen habe ich Einblick in die Teamstrukturen und Aufgabenbereiche von Entwicklern bekommen.";
+
+  const title3 = "Dies haben wir in Eigenarbeit geschaffen:";
+  const description3 = "Unter dem folgenden Link finden Sie mein Projekt.";
+
+  function setup(title, desc) {
+    $(".slideMain").css("width", "400px");
+    $(".slideTitle").text(title);
+    $(".slideDesc").text(desc);
+    setTimeout(function() {
+      $(".slideTitle, .slideDesc").css("color", "black");
+      $(".slideContent, .close").css("display", "block");
+    }, 1000);
+  };
 
   $(".img1").hover(function() {
-    /*animate sideContent and set text*/
-    $(".slideMain").css("width", "400px");
-    $(".slideTitle").text("Was ich gelernt habe:");
-    $(".slideDesc").text("Ich habe gelernt unter welchen Vorraussetzungen eine kommerzielle Website höhere Chanchen hat in diversen Suchmaschinen ein höheres Rating zu bekommen um mit mehr Klicks mehr Einkommen zu generieren sowie Grundlegende Kentnisse die benötig werden um einfache aber wirkende Seiten zu gestalten.");
-    /*after animation make stuff visible*/
-    setTimeout(function() {
-      $(".slideTitle, .slideDesc").css("color", "black");
-      $(".slideContent, .close").css("display", "block");
-    }, 1000);
-  });
-  $(".img2").hover(function() {
-    /*animate sideContent and set text*/
-    $(".slideMain").css("width", "400px");
-    $(".slideTitle").text("Arbeiten in der Web-Entwicklung:");
-    $(".slideDesc").text("In unserem Ausflug in ein Unternehmen habe ich Einblick in die Teamstrukturen und Aufgabenbereiche von Entwicklern bekommen.");
-    /*after animation make stuff visible*/
-    setTimeout(function() {
-      $(".slideTitle, .slideDesc").css("color", "black");
-      $(".slideContent, .close").css("display", "block");
-    }, 1000);
-  });
-  $(".img3").hover(function() {
-    /*animate sideContent and set text*/
-    $(".slideMain").css("width", "400px");
-    $(".slideTitle").text("Dies haben wir in Eigenarbeit geschaffen:");
-    $(".slideDesc").text("Unter dem folgenden Link finden Sie mein Projekt.");
-     $(".slideDesc").append( "<p><a href='https://sirricc.github.io/landingPage/'>Google</a></p>" );
-    /*after animation make stuff visible*/
-    setTimeout(function() {
-      $(".slideTitle, .slideDesc").css("color", "black");
-      $(".slideContent, .close").css("display", "block");
-    }, 1000);
+    setup(title1, description1);
   });
 
-  /*event listener to close sideContent*/
+  $(".img2").hover(function() {
+    setup(title2, description2);
+  });
+
+  $(".img3").hover(function() {
+    setup(title3, description3);
+    $(".slideDesc").append("<p><a href='https://sirricc.github.io/landingPage/'>Mein Projekt</a></p>");
+  });
+
   $(".close").click(function() {
     $(this).css("display", "none");
     $(".slideTitle, .slideDesc").css("color", "none");
     $(".slideContent, .close").css("display", "none");
     $(".slideMain").css("width", "20px");
-
-  })
-
+  });
 
 });
